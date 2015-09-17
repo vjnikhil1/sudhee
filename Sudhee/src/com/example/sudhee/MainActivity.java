64f2064c7e99d5d9413a80b9567f9a2241b4b2de
vjnikhil1 <johnnikhil95@gmail.com>
2015-09-17 16:36:50 +0530

    Final app ready for publishing

--- Sudhee/src/com/example/sudhee/MainActivity.java
+++ Sudhee/src/com/example/sudhee/MainActivity.java
@@ -1,36 +1,105 @@
 package com.example.sudhee;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.cbit.sudhee.R;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+	ActionBar actionbar;
+	 ViewPager viewpager;  
+     FragmentPageAdapter ft; 
+     
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		viewpager=(ViewPager)findViewById(R.id.pager);
+		ft = new FragmentPageAdapter(getSupportFragmentManager());
+		actionbar=getActionBar();
+		viewpager.setAdapter(ft);
+		actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS); 
+		actionbar.addTab(actionbar.newTab().setText("Kodemania").setTabListener(this));  
+        actionbar.addTab(actionbar.newTab().setText("Cognos").setTabListener(this));  
+        actionbar.addTab(actionbar.newTab().setText("Scintillate").setTabListener(this));
+        actionbar.addTab(actionbar.newTab().setText("Code Jumble").setTabListener(this));
+        actionbar.addTab(actionbar.newTab().setText("Tech Fun").setTabListener(this));
+        //actionbar.addTab(actionbar.newTab().setText("Lan Gaming").setTabListener(this));
+        actionbar.addTab(actionbar.newTab().setText("Gallery").setTabListener(this)); 
+        viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				actionbar.setSelectedNavigationItem(arg0);
+				
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	kya  hai re ghantesh dafuq
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		viewpager.setCurrentItem(tab.getPosition());
+		
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+	    switch (item.getItemId()) {
+	        case R.id.action_settings:
+	        	Intent intent=new Intent(this,About.class);
+	        	startActivity(intent);
+	        	return true;
+	            
+
+	        
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 }
